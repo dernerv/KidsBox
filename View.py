f6ef319d88c8cf4e45b3c34043776ca8a4fad2d6
Steffen Weber <steffen.weber@ibmt.fraunhofer.de>
2018-12-14 14:06:35 +0100

    AlbumCover wird nun dargestellt

--- View.py
+++ View.py
@@ -4,21 +4,43 @@ class View:
 
     def __init__(self, display):
         self.display = display
+        self.margin = 10
         pygame.font.init() # you have to call this at the start, 
                    # if you want to use this module.
-        self.font = pygame.font.SysFont('Century Gothic', 30) 
+        self.font = pygame.font.SysFont('Century Gothic', 40)
+        self.fontSmall = pygame.font.SysFont('Century Gothic', 16) 
     
     def Welcome(self):
-        textsurface = self.font.render('Kids Box v1.0', True, (200, 200, 200))
+        textKidsBox = self.font.render('Kids Box', True, (255, 255, 255))
+        textVersion = self.fontSmall.render('Version 1.0', True, (150, 150, 150))
         self.display.fill((0,0,0))
-        self.display.blit(textsurface,(40,160))
-        
+
+        centerX = self.display.get_width() / 2
+        centerY = self.display.get_height() / 2
+
+        # Render KidsBox
+        startKidTextX = (centerX- textKidsBox.get_width()/2)
+        startKidTextY = (centerY- textKidsBox.get_height()/2) - 20
+        self.display.blit(textKidsBox, (startKidTextX, startKidTextY))
+
+        #Render Version
+        startVersionTextX = (textKidsBox.get_width() - textVersion.get_width()) + startKidTextX
+        startVersionTextY = startKidTextY + textKidsBox.get_height()
+        self.display.blit(textVersion,(startVersionTextX,startVersionTextY))
+
         pygame.display.flip()
 
     def NewMedia(self, image, titel, position):
         self.image = image
         self.titel = titel
         self.position = position
+        
+        self.display.fill((110,110,110))
+        picture = pygame.transform.smoothscale(image, (300, 300))
+        self.display.blit(picture,(10,10))
+        textTitel = self.fontSmall.render(self.titel, True, (150, 150, 150))
+        self.display.blit(textTitel,(320,10))
+        pygame.display.flip()
     
     def UpdateTitle(self, titel, position):
         self.titel = titel
--- controller.py
+++ controller.py
@@ -3,17 +3,18 @@ import sys, signal
 
 from Player import Player
 from View import View
+from time import sleep
 
 class Controller:
     def __init__(self, display):
         self.display = display
-        
-    def loop(self):
-        clock = pygame.time.Clock()
-        
         self.player = Player()
         self.view = View(self.display)
         self.view.Welcome()
+        
+    def loop(self):
+        clock = pygame.time.Clock()
+
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -24,6 +25,8 @@ class Controller:
                     if event.key == pygame.K_s :
                         print("play file")
                         self.player.PlayFile("rise.mp3", 0.4)
+                        image = pygame.image.load("C:\\Users\\nerv\\sandbox\\Album1\\cover.png")
+                        self.view.NewMedia(image, "Test Titel", 0)
                     if event.key == pygame.K_p :
                         print("play / pause")
                         self.player.PlayPause()
